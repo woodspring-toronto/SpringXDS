78d213dd9e3ba57d9e4cb08765b4e9d55d7307e9
woodspring-toronto <32200982+woodspring-toronto@users.noreply.github.com>
2018-01-16 22:33:23 -0500

    Add files via upload

---
+++ src/main/java/woodspring/XdsSpring/SpringXdsApplication.java
@@ -0,0 +1,12 @@
+package woodspring.XdsSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringXdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringXdsApplication.class, args);
+	}
+}
